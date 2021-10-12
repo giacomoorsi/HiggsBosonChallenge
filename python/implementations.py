6b32fe9d577f50bb3d3770e75fe359b5d6deeacd
adfeel220 <adfeel220@gmail.com>
2021-10-12 18:06:56 +0200

    Make some codes look cleaner

--- python/implementations.py
+++ python/implementations.py
@@ -11,12 +11,6 @@ def compute_loss_mae(y, tx, w):
     """Calculate the loss using mean absolute error"""
     return sum(math.abs(y-np.matmul(tx,w)))/len(y)
 
-def compute_loss_ridge(y, tx, w, lambda_):
-    """Calculate the loss using ridge loss function"""
-    L = compute_loss_mse(y, tx, w)
-    omega = lambda_ * np.dot(w, w)
-    return (L + omega)
-
 def compute_loss(y,tx,w) : 
     return compute_loss_mse(y,tx,w)
 
@@ -80,7 +74,7 @@ def ridge_regression(y, tx, lambda_):
     diagonal_lambda = lambda_prime * np.identity(tx.shape[1])
     
     weight = np.linalg.solve(XX + diagonal_lambda, XY)
-    loss_mse = compute_loss_ridge(y, tx, weight, lambda_)
+    loss_mse = compute_loss(y, tx, weight)
 
     return loss_mse, weight
 

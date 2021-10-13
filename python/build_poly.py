261b1bfe0cf96cfb71667cadfdb93fba4bea10a4
Giacomo Orsi <orsi.giacomo@gmail.com>
2021-10-13 11:48:40 +0200

    Project files, helpers, datasets

--- python/build_poly.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""implement a polynomial basis function."""
-
-import numpy as np
-
-def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    return np.array([[pow(x[i],j) for j in range(0, degree+1)] for i in range(0, len(x))])

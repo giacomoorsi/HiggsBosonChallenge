6ef1865432b1fe27347ca3a3a7786f8e7b7c4bef
Giacomo Orsi <orsi.giacomo@gmail.com>
2021-10-31 11:23:37 +0100

    Code cleaning

--- scripts/logistic_regression_helpers.py
+++ scripts/logistic_regression_helpers.py
@@ -29,27 +29,27 @@ def sample_data(y, x, seed, size_samples):
     return y[:size_samples], x[:size_samples]
 
 
-def standardize(x):
-    """Standardize the original data set."""
-    mean_x = np.mean(x, axis=0)
-    x = x - mean_x
-    std_x = np.std(x, axis=0)
-    x = x / std_x
-    return x, mean_x, std_x
+# def standardize(x):
+#     """Standardize the original data set."""
+#     mean_x = np.mean(x, axis=0)
+#     x = x - mean_x
+#     std_x = np.std(x, axis=0)
+#     x = x / std_x
+#     return x, mean_x, std_x
 
 
-def de_standardize(x, mean_x, std_x):
-    """Reverse the procedure of standardization."""
-    x = x * std_x
-    x = x + mean_x
-    return x
+# def de_standardize(x, mean_x, std_x):
+#     """Reverse the procedure of standardization."""
+#     x = x * std_x
+#     x = x + mean_x
+#     return x
 
 
-def build_model_data(height, weight):
-    """Form (y,tX) to get regression data in matrix form."""
-    y = weight
-    x = height
-    num_samples = len(y)
-    tx = np.c_[np.ones(num_samples), x]
-    return y, tx
+# def build_model_data(height, weight):
+#     """Form (y,tX) to get regression data in matrix form."""
+#     y = weight
+#     x = height
+#     num_samples = len(y)
+#     tx = np.c_[np.ones(num_samples), x]
+#     return y, tx
 

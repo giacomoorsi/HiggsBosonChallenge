4535cb6dd3739ca54b1fbf61c01da58cf94dce4a
Vittorio Rossi <vittgiac@gmail.com>
2021-10-31 23:02:41 +0100

    Fixed bugs

--- scripts/run.py
+++ scripts/run.py
@@ -3,23 +3,26 @@ from models import compute_weights, compute_predictions
 from proj1_helpers import create_csv_submission, load_csv_data
 
 models = {
- "jet0" : {
+    "jet0" : {
         "model" : "least squares",
-        "degree" : 7,
-        "lambda" : 1e-5,
-        "mixed" : False,
+        "degree" : 4,
+        "lambda" : 1e-10,
+        "mixed" : True,
+        "accuracy" : 0.8470
     },
     "jet1" : {
         "model" : "least squares",
-        "degree" : 7,
-        "lambda" : 1e-4,
-        "mixed" : False,
+        "degree" : 6,
+        "lambda" : 1e-3,
+        "mixed" :  True,
+        "accuracy" : 0.8069
     },
     "jet2" : {
         "model" : "least squares",
-        "degree" : 7,
+        "degree" : 4,
         "lambda" : 1e-4,
-        "mixed" : False,
+        "mixed" : True,
+        "accuracy" : 0.8342
     }
 }
 
@@ -31,13 +34,13 @@ def main() :
     JET_COLUMN_INDEX = 22
     OUTPUT_PATH = "out.csv"
 
-    y, tX, _ = load_csv_data(DATA_TRAIN_PATH)
-    y_jets, x_jets, replacing_values, means, stds = prepare_train_data(tX, y)
+    y, tX, _ = load_csv_data(DATA_TRAIN_PATH, sub_sample=False)
+    y_jets, x_jets, means, stds, replacing_values = prepare_train_data(y, tX)
 
     w_jets = compute_weights(x_jets, y_jets, models)
 
-    _, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
-    x_test_jets = prepare_test_data(replacing_values, means, stds)
+    _, tX_test, ids_test = load_csv_data(DATA_TEST_PATH, sub_sample=False)
+    x_test_jets = prepare_test_data(tX_test, means, stds, replacing_values)
 
     y_pred = compute_predictions(x_test_jets, w_jets, models, tX_test[:, JET_COLUMN_INDEX])
 

afd3a62ba13d7739b7368fca2cfec080afd6121a
Giacomo Orsi <orsi.giacomo@gmail.com>
2021-10-31 23:11:22 +0100

    Project files reordering

--- scripts/plots.py
+++ scripts/plots.py
@@ -1,4 +1,13 @@
 from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+matplotlib.rcParams.update({'font.size': 22})
 
 
 def plot_degree_errors_plt(degrees, lambdas, results):
@@ -8,8 +17,11 @@ def plot_degree_errors_plt(degrees, lambdas, results):
         accuracy[accuracy == 0] = 'nan'
         plt.plot(lambdas, accuracy, label=f"{deg}")
     plt.xscale('log')
-    plt.xlabel('λ')
+    plt.title("least squares on jet0")
+    plt.xlabel('$\lambda$')
     plt.ylim([0.7, 0.9])
     plt.ylabel('Accuracy')
     leg = plt.legend(loc='best', title="Degrees")
     leg.get_frame().set_alpha(0.3)
+    plt.savefig('plot-.pgf')
+
